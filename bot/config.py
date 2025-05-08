ed3c8c4526d4318a38b5a50dc970c8e7073cd21a
pipiska22754 <izzqwar@icloud.com>
2025-05-08 22:54:04 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-def read_list(filename: str) -> list:
+def load_list(filename: str) -> list:
     try:
         with open(filename, 'r') as f:
             return [line.strip() for line in f if line.strip()]
@@ -12,6 +9,5 @@ def read_list(filename: str) -> list:
 
 API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
-PHONE = os.getenv('PHONE')
-ADMINS = read_list('admins.txt')
-GROUPS = read_list('groups.txt')
+ADMINS = load_list('admins.txt')
+GROUPS = load_list('groups.txt')
